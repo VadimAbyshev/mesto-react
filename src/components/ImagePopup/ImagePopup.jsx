d41f6d56ff0de11ea12b50efca606fc97bc24b5b
VadimAbyshev <boss.abyshev@gmail.com>
2023-06-22 22:16:46 +0500

    пОпАп

--- src/components/ImagePopup/ImagePopup.jsx
+++ src/components/ImagePopup/ImagePopup.jsx
@@ -1,4 +1,4 @@
-export default function PopupImage({card, isOpen, onClose}){
+export default function ImagePopup({card, isOpen, onClose}){
     return (
         <div className= {`popup popup_open-card  ${isOpen && 'popup_opened'}`}>
         <figure className="popup__figure-card">
